e34fcedaf635d9d6e83042a75dba7a9838634144
AMCNL <am_cnl@163.com>
2020-08-14 01:09:17 +0800

    新增eureka

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.sql.SQLOutput;
+
+/**
+ * Date:2020/8/14
+ * Author:Lynn.cn.Li
+ * Desc:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
