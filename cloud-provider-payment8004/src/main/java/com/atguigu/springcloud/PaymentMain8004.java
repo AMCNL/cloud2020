23b6f66a8e48c81e009785ffea18fc16f04a598a
AMCNL <am_cnl@163.com>
2020-09-22 00:21:19 +0800

    功能完善

--- cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -12,7 +13,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class PaymentMain8004 {
     public static void main(String[] args) {
-
-
+        SpringApplication.run(PaymentMain8004.class,args);
     }
 }
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * Date:2020/9/21
+ * Author:Lynn.cn.Li
+ * Desc:
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk()
+    {
+        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
