f5c1dd11708d1e7e380b59f42f1035c58e1a3e34
AMCNL <am_cnl@163.com>
2020-09-21 21:58:51 +0800

    功能完善

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Date:2020/8/27
+ * Author:Lynn.cn.Li
+ * Desc:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+
+
+    }
+}
