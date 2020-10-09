e235ea3a972ccf3e5df184430b47dd8837e04fe2
AMCNL <am_cnl@163.com>
2020-10-10 00:44:58 +0800

    功能完善

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,30 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Date:2020/10/9
+ * Author:Lynn.cn.Li
+ * Desc:
+ */
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Resource
+    PaymentService paymentService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        System.out.println("1111");
+        return  paymentService.getPaymentById(id);
+    }
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Date:2020/10/9
+ * Author:Lynn.cn.Li
+ * Desc:
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);//@PathVariable这个注解可以把@GetMapping(value = "/payment/get/{id}")中id的值进行绑定
+
+}
