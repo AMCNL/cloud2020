0e8e78d2b9c35a2e9a9581be898ca0b05bdb12ae
lichunnan <li.chunnan@xyb2b.com>
2020-10-10 22:27:39 +0800

    功能完善

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/springcloud/OrderFeignMain80.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * Desc:
  */
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 public class OrderFeignMain80 {
     public static void main(String[] args) {
